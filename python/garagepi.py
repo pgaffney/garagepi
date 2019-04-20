afd423ddcdc9630eed7be94cf94c5b092f9f3394
Paul Gaffney <pgaffney2000@gmail.com>
2019-04-20 13:33:45 +0100

    initial commit

--- python/garagepi.py
+++ python/garagepi.py
@@ -2,20 +2,23 @@ import webiopi
 
 GPIO = webiopi.GPIO
 
-#GPIO.setmode(GPIO.BCM)
-
-DOOR = 4 # GPIO pin using BCM numbering
+DOOR1 = 4
+DOOR2 = 17
 
 # setup function is automatically called at WebIOPi startup
 def setup():
     # set the GPIO used by the door to output
-    GPIO.setFunction(DOOR, GPIO.OUT)
+    GPIO.setFunction(DOOR1, GPIO.OUT)
+    GPIO.setFunction(DOOR2, GPIO.OUT)
     return
 
-# loop function is repeatedly called by WebIOPi 
+# loop function is repeatedly called by WebIOPi
 def loop():
     return
 
 # destroy function is called at WebIOPi shutdown
 def destroy():
-    GPIO.digitalWrite(DOOR, GPIO.HIGH)
+    GPIO.digitalWrite(DOOR1, GPIO.HIGH)
+    GPIO.digitalWrite(DOOR2, GPIO.HIGH)
+    GPIO.cleanup()
+    return
